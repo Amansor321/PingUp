4b07973bd215b48617006695ba927a77c96b3675
Amansor321 <soroutaman321@gmail.com>
2025-12-13 12:05:24 +0530

    email reminder added

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/user.js";
+import Connection from "../models/connection.js";
+import sendEmail from "../config/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
@@ -15,10 +17,9 @@ const syncUserCreation = inngest.createFunction(
   },
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
-      event.data
+      event.data;
 
-    let username = 
-      email_addresses[0].email_address.split('@')[0];
+    let username = email_addresses[0].email_address.split("@")[0];
 
     //CHECK AVAILABILTY OF USERNAME
 
@@ -77,5 +78,96 @@ const syncUserDeletion = inngest.createFunction(
   }
 );
 
+const sendNewConnectionRequestReminder = inngest.createFunction(
+  {
+    id: "send-new-connection-request-reminder",
+  },
+  {
+    event: "app/connection-request",
+  },
+  async ({ event, step }) => {
+    const { connectionId } = event.data;
+    await step.run("send-connection-request-mail", async () => {
+      const connection = await Connection.findById(connectionId).populate(
+        "from_user_id to_user_id"
+      );
+
+      const subject = `New connection request`;
+      const body = `
+        <div style="font-family: Arial, sans-serif; padding: 20px;">
+  <h2>Hi ${connection.to_user_id.full_name},</h2>
+  <p>
+    You have a new connection request from
+    ${connection.from_user_id.full_name} @${connection.from_user_id.username}
+  </p>
+  <p>
+    Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#1b0981;">here</a>
+    to accept or reject the request
+  </p>
+  <br/>
+  <p>Thanks,<br/>PingUp - Stay Connected</p>
+</div>
+
+        `;
+
+      await sendEmail({
+        to: connection.to_user_id.email,
+        subject,
+        body,
+      });
+    });
+
+    const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
+    await step.sleepUntil("wait-for-24-hours", in24Hours);
+    await step.run("send-connection-request-reminder", async () => {
+      const connection = await Connection.findById(connectionId).populate(
+        "from_user_id to_user_id"
+      );
+
+      if (connection.status === "accepted") {
+        return { message: "Already acceptef" };
+      }
+
+
+        const subject = `New connection request`;
+      const body = `
+        <div style="font-family: Arial, sans-serif; padding: 20px;">
+  <h2>Hi ${connection.to_user_id.full_name},</h2>
+  <p>
+    You have a new connection request from
+    ${connection.from_user_id.full_name} @${connection.from_user_id.username}
+  </p>
+  <p>
+    Click <a href="${process.env.FRONTEND_URL}/connections" style="color:#1b0981;">here</a>
+    to accept or reject the request
+  </p>
+  <br/>
+  <p>Thanks,<br/>PingUp - Stay Connected</p>
+</div>
+
+        `;
+
+      await sendEmail({
+        to: connection.to_user_id.email,
+        subject,
+        body,
+      });
+
+      return {message:"Remainder send"}
+
+
+
+
+
+
+
+
+
+    });
+  }
+);
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [syncUserCreation, syncUserUpdation,syncUserDeletion];
+export const functions = [syncUserCreation, syncUserUpdation, syncUserDeletion,
+  sendNewConnectionRequestReminder
+];
--- server/server.js
+++ server/server.js
@@ -4,12 +4,15 @@ import 'dotenv/config';
 import connectDB from './config/db.js';
 import {inngest,functions} from './inngest/index.js';
 import { serve } from "inngest/express";
+import { clerkMiddleware } from '@clerk/express'
+import userRouter from './routes/userRoutes.js';
 const app= express();
 
 await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
 
 
 app.get('/',(req,res)=>{
@@ -17,6 +20,7 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/api/inngest', serve({ client: inngest, functions }))
+app.use('/api/user',userRouter);
 
 
 const PORT= process.env.PORT || 4000;
